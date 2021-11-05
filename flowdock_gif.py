8e53b3fa7afc5f3e8cc87fc2659d5f60dd3de8af
RomainMou <58464216+RomainMou@users.noreply.github.com>
2021-11-05 12:26:47 +0100

    Better code, I hope.

--- flowdock_gif.py
+++ flowdock_gif.py
@@ -1,7 +1,6 @@
 #! /usr/bin/python3
 
 import sys
-import json
 import requests
 import argparse
 from pathlib import Path
@@ -12,33 +11,28 @@ config = dotenv_values("{}/.env".format(Path(__file__).parent.absolute()))
 
 # Parse arguments
 parser = argparse.ArgumentParser()
-parser.add_argument("-u", "--user", help="Flowdock user recipient id", type=int, required=True)
-parser.add_argument("-t", "--tag", help="Giphy tag for the random search", type=str, default="unicorn")
+parser.add_argument(
+    "-u", "--user", help="Flowdock user recipient id", type=int, required=True
+)
+parser.add_argument(
+    "-t", "--tag", help="Giphy tag for the random search", type=str, default="unicorn"
+)
 args = parser.parse_args()
 
 # Get a random gif
 gif_r = requests.get(
-    'https://api.giphy.com/v1/gifs/random',
-    params = {
-        "tag": args.tag,
-        "api_key": config['GIPHY_API_KEY'],
-        "rating": "pg-13"
-    }
+    "https://api.giphy.com/v1/gifs/random",
+    params={"tag": args.tag, "api_key": config["GIPHY_API_KEY"], "rating": "pg-13"},
 )
 
-gif_r_dict = json.loads(gif_r.text)
-
-if gif_r_dict['data']:
-    gif_url = gif_r_dict['data']['images']['original']['url'].split('?')[0]
-    print("Sending '{}' gif.".format(gif_url))
+if gif_r_dict := gif_r.json()["data"]:
+    gif_url = gif_r_dict["images"]["original"]["url"].split("?")[0]
+    print(f"Sending {gif_url} gif.")
     requests.post(
-       "https://api.flowdock.com/private/{}/messages".format(args.user),
-       auth = (config['FLOWDOCK_API_TOKEN'], ''),
-       json = {
-           "event": "message",
-           "content": "/giphy " + args.tag + "\n" + gif_url
-       }
+        f"https://api.flowdock.com/private/{args.user}/messages",
+        auth=(config["FLOWDOCK_API_TOKEN"], ""),
+        json={"event": "message", "content": f"/giphy {args.tag} \n{gif_url}"},
     )
 else:
-    print("No gif found for '{}'.".format(args.tag), file=sys.stderr)
+    print(f"No gif found for '{args.tag}'.", file=sys.stderr)
     sys.exit(1)
